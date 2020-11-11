dfba73ca0ced85b9085f0492f3bbecb6a0d47486
Hannahvic <hannahvl1987@hotmail.com>
2020-11-11 18:52:55 +0000

    Updates

---
+++ precache-manifest.0f3bde50bcb7b74a88b51db0ee15084e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a401be86499f31dddc12e6a3376714c",
+    "url": "/faceRecognition/index.html"
+  },
+  {
+    "revision": "e7516dd727bea356edf1",
+    "url": "/faceRecognition/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "1a664c70bd13f5519a7e",
+    "url": "/faceRecognition/static/css/main.043b4fd3.chunk.css"
+  },
+  {
+    "revision": "e7516dd727bea356edf1",
+    "url": "/faceRecognition/static/js/2.22018993.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/faceRecognition/static/js/2.22018993.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a664c70bd13f5519a7e",
+    "url": "/faceRecognition/static/js/main.23442ca6.chunk.js"
+  },
+  {
+    "revision": "0c9c30fd9d963eb99a8e",
+    "url": "/faceRecognition/static/js/runtime-main.54299cbe.js"
+  },
+  {
+    "revision": "6fb51fc170e8d766a0dd18335142a141",
+    "url": "/faceRecognition/static/media/face.6fb51fc1.svg"
+  }
+]);
